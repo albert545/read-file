4cc328d351ce3efd9499bcd3f51198e1aca9b25d
Albert Liao <pulieden@gmail.com>
2018-03-03 09:33:13 +0800

    印出讀取進度與與1000筆才印一次

--- read.py
+++ read.py
@@ -6,9 +6,13 @@ Created on Sat Mar  3 09:11:36 2018
 """
 
 data = []
+count = 0
 with open('reviews.txt','r') as f:
     for line in f:
         data.append(line)
+        count +=1
+        if count % 10000 == 0:
+            print(len(data))
 print(len(data))
 
 print(data[0])
